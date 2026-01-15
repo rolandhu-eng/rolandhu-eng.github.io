4d9989e273e3a3be25fc2a53c140afb1e6ead33b
Roland <bintan@iMac-2029.local>
2026-01-14 18:06:09 -0600

    updated look

--- js/main.js
+++ js/main.js
@@ -40,3 +40,15 @@ const observer = new IntersectionObserver((entries) => {
 }, observerOptions);
 
 document.querySelectorAll('.fade-in').forEach(el => observer.observe(el));
+
+//parallax with plume
+window.addEventListener('scroll', function(){
+const target = document.querySelector('.hero');
+
+// Calculate how many pixels we have scrolled
+let scrolled = window.pageYOffset;
+
+// Adjust the '0.5' to change the speed (0.2 is slower, 0.8 is faster)
+// The negative sign moves it UP as you scroll DOWN
+target.style.backgroundPositionY = -(scrolled * 1.5) + 'px';
+});
